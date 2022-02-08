3f8baa8c8e2f78cb5d7f29a0875a9048826afae9
José <joseantoniocastrom5@gmail.com>
2022-02-08 17:24:13 -0500

    Fiz modificações do arquivo de Java 2D

--- 02-Java2D-Hello/Hello2DApp.java
+++ 02-Java2D-Hello/Hello2DApp.java
@@ -1,6 +1,10 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.awt.Color;
+import java.awt.image.*;
+import java.io.*;
+import javax.imageio.*;
 
 public class Hello2DApp {
     public static void main (String[] args) {
@@ -9,6 +13,9 @@ public class Hello2DApp {
 }
 
 class Hello2DFrame extends JFrame {
+
+    BufferedImage img;
+
     public Hello2DFrame () {
         this.addWindowListener (
             new WindowAdapter() {
@@ -18,17 +25,56 @@ class Hello2DFrame extends JFrame {
             }
         );
         this.setTitle("Java2D - Hello World!");
-        this.setSize(350, 350);
+        this.getContentPane().setBackground(Color.blue);
+        this.setLocationRelativeTo(null);
+        this.setSize(500, 600);
         this.setVisible(true);
+        try {
+            img = ImageIO.read(new File("Happy_smiley_face.png"));
+        } catch (IOException e) {
+        }
     }
 
     public void paint (Graphics g) {
         super.paint(g);
         Graphics2D g2d = (Graphics2D) g;
-        g2d.setPaint(Color.blue);
-        int w = getWidth();
-        int h = getHeight();
-        g2d.drawLine(0,0, w,h);
-        g2d.drawLine(0,h, w,0);
+        //Desenho da casa
+        g2d.setPaint(Color.red);
+        g2d.fillRect(100,375,200,200);
+        g2d.setPaint(Color.black);
+        g2d.drawRect(100,375,200,200);
+        //Caracteristicas da palavra
+        g2d.setPaint(Color.magenta);
+        g2d.setFont(new Font("Ink Free",Font.BOLD,50));
+        g2d.drawString("This is my house",50,120);
+
+        //Desenho do sol
+        g2d.setPaint(Color.yellow);
+        g2d.fillOval(100,150,75,75);
+        g2d.setPaint(Color.black);
+        g2d.drawOval(100,150,75,75);
+
+        //Desenho da grama
+        g2d.setPaint(Color.green);
+        g2d.fillRect(0,575,500,25);
+
+        //Desenho do teto da casa
+        g2d.setPaint(Color.black);
+        Polygon poly = new Polygon();
+        poly.addPoint(100,375);
+        poly.addPoint(300,375);
+        poly.addPoint(200,300);
+        g2d.drawPolygon(poly);
+        g2d.fillPolygon(poly);
+
+        //Desenho da porta
+        g2d.fillRect(175,525,50,50);
+
+        g2d.setStroke(new BasicStroke(5));
+        g2d.drawLine(0,575,500,575);
+
+        //Imagem
+        g2d.drawImage(img,425,75,50,50,null);
+
     }
 }
