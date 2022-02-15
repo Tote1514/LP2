acfeac9af27d2596d713b7a93e6048b3fcd14d57
José <joseantoniocastrom5@gmail.com>
2022-02-15 15:19:38 -0500

    Modifiquei o arquivo RectApp.java implementando os métodos

--- 02-Java2D-Hello/RectApp.java
+++ 02-Java2D-Hello/RectApp.java
@@ -2,6 +2,11 @@ public class RectApp {
     public static void main (String[] args) {
         Rect r1 = new Rect(1,1, 10,10);
         r1.print();
+        //testes
+        int x = r1.area();
+        r1.drag(5,7);
+        System.out.println("Área = "+x);
+        r1.print();
     }
 }
 class Rect {
@@ -17,4 +22,13 @@ class Rect {
         System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
             this.w, this.h, this.x, this.y);
     }
+    int area() {
+        return w*h;
+    }
+    void drag(int dx, int dy){
+        this.x = this.x + dx;
+        this.y = this.y + dy;
+    }
+
+
 }
