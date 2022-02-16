6edd74a323b166fdd840710edb508c1f6644679b
José <joseantoniocastrom5@gmail.com>
2022-02-16 15:28:44 -0500

    Modifiquei um arquivo

--- 02-Java2D-Hello/Elipse.c
+++ 02-Java2D-Hello/Elipse.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 typedef struct {
 	int x, y;//posição do canto superior de onde vai ser desenhado circulo, não é a posição do centro
-	int raio;// o raio do circulo
+	int w, h;//Dimensões do circulo
 	int r, g, b; // a cor do circulo
 	int rota; // o angulo de rotação do circulo
 }Circulo;
 
 void print(Circulo* c){
-	printf("O circulo de raio (%d) e posição (%d,%d)",c->raio,c->x,c->y);
+	printf("A elipse de Dimensões(%d,%d) e posição (%d,%d)",c->w,c->h,c->x,c->y);
 }
 
 int main() {
-	Circulo c1 = {150,200,50,0,0,0,45};
+	Circulo c1 = {150,200,50,50,0,0,0,45};
 	print(&c1);
 	return 0;
 }
