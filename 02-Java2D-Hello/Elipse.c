737f6f1780f6910d4baa0f6841705db16398e5b9
José <joseantoniocastrom5@gmail.com>
2022-02-16 15:34:56 -0500

    Fiz modificações no arquivo de elipse

--- 02-Java2D-Hello/Elipse.c
+++ 02-Java2D-Hello/Elipse.c
@@ -4,14 +4,14 @@ typedef struct {
 	int w, h;//Dimensões do circulo
 	int r, g, b; // a cor do circulo
 	int rota; // o angulo de rotação do circulo
-}Circulo;
+}Elipse;
 
-void print(Circulo* c){
-	printf("A elipse de Dimensões(%d,%d) e posição (%d,%d)",c->w,c->h,c->x,c->y);
+void print(Elipse* e){
+	printf("A elipse de Dimensões(%d,%d) e posição (%d,%d)",e->w,e->h,e->x,e->y);
 }
 
 int main() {
-	Circulo c1 = {150,200,50,50,0,0,0,45};
-	print(&c1);
+	Elipse elip1 = {150,200,50,50,0,0,0,45};
+	print(&elip1);
 	return 0;
 }
