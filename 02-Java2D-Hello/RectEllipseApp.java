b9c78ada7a4ec41fcb938464d2b0a23188289970
José <joseantoniocastrom5@gmail.com>
2022-02-16 19:53:48 -0500

    Modifiquei o arquivo RectEllipses.java

--- 02-Java2D-Hello/RectEllipseApp.java
+++ 02-Java2D-Hello/RectEllipseApp.java
@@ -13,7 +13,7 @@ class RectEllipseApp {
 
 class RectEllipseFrame extends JFrame {
     Rect r1;
-    Ellipse e1;
+    Ellipse e1, e2, e3;
 
     RectEllipseFrame () {
         this.addWindowListener (
@@ -24,27 +24,39 @@ class RectEllipseFrame extends JFrame {
             }
         );
         this.setTitle("Rect + Ellipse");
-        this.setSize(350, 350);
-        this.r1 = new Rect(50,50, 100,30);
-        this.e1 = new Ellipse(50,100, 100,30);
+        this.setSize(600,600);
+        this.e1 = new Ellipse(50,50, 100,30,0,0,0,255,117,24);
+        this.e2 = new Ellipse(100,200,250,100,255,0,0,253,233,16);
+        this.e3 = new Ellipse(200,350,100,200,128,0,0,0,191,255);
     }
 
     public void paint (Graphics g) {
         super.paint(g);
-        this.r1.paint(g);
         this.e1.paint(g);
+        this.e2.paint(g);
+        this.e3.paint(g);
     }
 }
 
 class Rect {
     int x, y;
     int w, h;
+    //Cor de contorno
+    int r1, g1, b1;
+    //Cor de fundo
+    int r2, g2, b2;
 
-    Rect (int x, int y, int w, int h) {
+    Rect (int x, int y, int w, int h,int r1, int g1, int b1, int r2, int g2, int b2) {
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
+        this.r1 = r1;
+        this.g1 = g1;
+        this.b1 = b1;
+        this.g2 = g2;
+        this.r2 = r2;
+        this.b2 = b2;
     }
 
     void print () {
@@ -53,19 +65,33 @@ class Rect {
     }
 
     void paint (Graphics g) {
-        g.drawRect(this.x,this.y, this.w,this.h);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setColor(new Color(this.r1, this.g1, this.b1));
+        g2d.drawRect(this.x,this.y, this.w,this.h);
+        g2d.setColor(new Color(this.r2, this.g2, this.b2));
+        g2d.fillRect(this.x,this.y, this.w,this.h);
     }
 }
 
 class Ellipse {
     int x, y;
     int w, h;
+    //Cor de contorno
+    int r1, g1, b1;
+    //Cor de fundo
+    int r2, g2, b2;
 
-    Ellipse (int x, int y, int w, int h) {
+    Ellipse (int x, int y, int w, int h,int r1, int g1, int b1, int r2, int g2, int b2) {
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
+        this.r1 = r1;
+        this.g1 = g1;
+        this.b1 = b1;
+        this.g2 = g2;
+        this.r2 = r2;
+        this.b2 = b2;
     }
 
     void print () {
@@ -75,6 +101,9 @@ class Ellipse {
 
     void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-        g2d.draw(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
+        g2d.setColor(new Color(this.r1, this.g1, this.b1));
+        g2d.drawOval(this.x,this.y, this.w,this.h);
+        g2d.setColor(new Color(this.r2, this.g2, this.b2));
+        g2d.fillOval(this.x,this.y, this.w,this.h);
     }
 }
