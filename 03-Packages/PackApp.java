d343ec379d5153a4be7c6ae9d939a975feeac922
José <joseantoniocastrom5@gmail.com>
2022-02-28 16:18:34 -0500

    Modifiquei o arquivo PackApp.java

--- 03-Packages/PackApp.java
+++ 03-Packages/PackApp.java
@@ -1,22 +1,26 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Random;
 
 import figures.*;
 
 class PackApp {
     public static void main (String[] args) {
-        PackFrame frame = new PackFrame();
+        ListFrame frame = new ListFrame();
         frame.setVisible(true);
     }
 }
 
-class PackFrame extends JFrame {
-    Rect r1;
-    Ellipse e1;
-    Triangle t1;
+class ListFrame extends JFrame {
+    ArrayList<Rect> rectangulos = new ArrayList<Rect>();
+    ArrayList<Ellipse> elipses = new ArrayList<Ellipse>();
+    Random random = new Random();
+    final int COLOR = 256;//Parametro para passar de argumento para gerar uma cor aleatoria
 
-    PackFrame () {
+
+    ListFrame () {
         this.addWindowListener (
             new WindowAdapter() {
                 public void windowClosing (WindowEvent e) {
@@ -24,17 +28,35 @@ class PackFrame extends JFrame {
                 }
             }
         );
-        this.setTitle("Java Packages");
+        this.addKeyListener (
+            new KeyAdapter() {
+                public void keyPressed (KeyEvent evt) {
+                    if (evt.getKeyChar() == 'e') {
+                        int x = random.nextInt(600);
+                        int y = random.nextInt(600);
+                        int w = random.nextInt(50)+1;//Somar um para não dar um largura de zero
+                        int h = random.nextInt(50)+1;//Somar um para não dar um altura de zero
+                        int r1 = random.nextInt(COLOR);
+                        int g1 = random.nextInt(COLOR);
+                        int b1 = random.nextInt(COLOR);
+                        int r2 = random.nextInt(COLOR);
+                        int g2 = random.nextInt(COLOR);
+                        int b2 = random.nextInt(COLOR);
+                        elipses.add(new Ellipse(x,y, w,h,r1,g1,b1,r2,g2,b2));
+                        repaint();  // outer.repaint()
+                    }
+                }
+            }
+        );
+        this.setTitle("Lista de Elipses e Rectangulos");
         this.setSize(600, 600);
-        this.r1 = new Rect(50,50, 100,30,128,0,0,255,117,24);
-        this.e1 = new Ellipse(200,200, 100,30,128,0,0,255,117,24);
-        this.t1 = new Triangle(100,300,200,375,375,300,128,0,0,255,117,24);
+
     }
 
     public void paint (Graphics g) {
         super.paint(g);
-        this.r1.paint(g);
-        this.e1.paint(g);
-        this.t1.paint(g);
+        for (Ellipse e : this.elipses) {
+            e.paint(g);
+        }
     }
 }
